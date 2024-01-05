34e97a5e008b2f5905b369cc63ff5bc5cb2423af
lucero changra <luc3r0.ch4ngr4.99@gmail.com>
2024-01-05 10:10:04 -0500

    entidades dao

---
+++ src/main/java/model/Servicio.java
@@ -0,0 +1,50 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author Lucero
+ */
+public class Servicio {
+    
+    private int idServicio;
+    private String descripcion;
+    private double precio;
+    
+    public Servicio(int idServicio, String descripcion, double precio){
+        this.idServicio=idServicio;
+        this.descripcion=descripcion;
+        this.precio=precio;
+    }
+
+    public int getIdServicio() {
+        return idServicio;
+    }
+
+    public void setIdServicio(int idServicio) {
+        this.idServicio = idServicio;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+    
+    
+    
+    
+}
